c7f51718793ff620c541c3b2873b041caadb9d6c
HasanDevOps2003 <muhammedhasan483@gmail.com>
2022-11-12 17:23:57 +0000

    outputs for components created in vpc

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value = module.vpc.vpc_id
+}
+
+output "vpc_arn" {
+  description = "The arn of the VPC"
+  value = module.vpc.vpc_arn
+}
+
+output "public_subnet_id" {
+  description = "The public subnet ID"
+  value = module.vpc.public_subnets
+}
+
+output "public_subnet_arn" {
+  description = "The public subnet arn"
+  value = module.vpc.public_subnet_arns
+}
+
+output "public_subnet_cidr_block" {
+  description = "The public subnet CIDR block"
+  value = module.vpc.public_subnets_cidr_blocks
+}
