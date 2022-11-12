d5be3d8eb62b2967836519fed383c7abb995f9b7
HasanDevOps2003 <muhammedhasan483@gmail.com>
2022-11-12 22:33:37 +0000

    outputs for instance security group

--- outputs.tf
+++ outputs.tf
@@ -24,11 +24,21 @@ output "public_subnet_cidr_block" {
 }
 
 output "instance_ssh_key_arn" {
-  description = "The arn of the instance ssh key paid"
+  description = "The arn of the instance ssh key pair"
   value = aws_key_pair.instance_ssh_key.arn
 }
 
 output "instance_ssh_key_id" {
-  description = "The ID of the instance ssh key paid"
+  description = "The ID of the instance ssh key pair"
   value = aws_key_pair.instance_ssh_key.id
+}
+
+output "instance_sg_arn" {
+  description = "The arn of the instance security group"
+  value = aws_security_group.instance_sg.arn
+}
+
+output "instance_sg_id" {
+  description = "The ID of the instance security group"
+  value = aws_security_group.instance_sg.id
 }
